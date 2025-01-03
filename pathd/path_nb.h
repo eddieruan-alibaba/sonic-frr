guoguojia2021/frr

pathd: pathd support IPv6 address SID list
command:

router2(config-sr-te)# segment-list test_srv6
router2(config-sr-te-segment-list)# index 10 ipv6-address
  X:X::X:X  IPv6 address
router2(config-sr-te-segment-list)# index 10 ipv6-address 100:1::1
router2(config-sr-te-segment-list)# index 20 ipv6-address 200:1::1
router2(config-sr-te-segment-list)# index 30 mpls label
  (16-1048575)  Label Value
router2(config-sr-te-segment-list)# index 30 mpls label 100
% Configuration failed.

Error type: validation
Error description: The Segment List(test_srv6) Type must be SRv6!
router2(config-sr-te-segment-list)#
router2(config-sr-te)# segment-list test_mpls
router2(config-sr-te-segment-list)# index 10 mpls label 100
router2(config-sr-te-segment-list)# index 20 mpls label 200
router2(config-sr-te-segment-list)# index 30 ipv6-address 1:1::1
% Configuration failed.

Error type: validation
Error description: The Segment List(test_mpls) Type must be MPLS!
router2(config-sr-te-segment-list)#

router2# configure
router2(config)# seg
router2(config-sr)# traffic-eng
router2(config-sr-te)# policy color 100 endpoint
  A.B.C.D   SR Policy endpoint IPv4 address
  X:X::X:X  SR Policy endpoint IPv6 address
router2(config-sr-te)# policy color 100 endpoint 1.1.1.1
  <cr>
router2(config-sr-te)# policy color 100 endpoint 1.1.1.1
router2(config-sr-te-policy)# candidate-path preference 1 name mpls explicit segment-list test_mpls
  <cr>
router2(config-sr-te-policy)# candidate-path preference 1 name mpls explicit segment-list test_mpls
router2(config-sr-te-policy)# candidate-path preference 2 name srv6 explicit segment-list test_srv6
% Configuration failed.

Error type: validation
Error description: The Segment List type(2) and Policy type(1) must match!
router2(config-sr-te-policy)# exit
router2(config-sr-te)# policy color 200 endpoint 1:1::1
router2(config-sr-te-policy)# candidate-path preference 1 name srv6 explicit segment-list test_srv6
router2(config-sr-te-policy)# candidate-path preference 2

pathd/path_nb.h
@@ -21,6 +21,8 @@ pathd_srte_segment_list_lookup_entry(struct nb_cb_lookup_entry_args *args);
 
 int pathd_srte_segment_list_segment_create(struct nb_cb_create_args *args);
 int pathd_srte_segment_list_segment_destroy(struct nb_cb_destroy_args *args);
+int pathd_srte_segment_list_segment_pre_validate(
+	struct nb_cb_pre_validate_args *args);
 int pathd_srte_segment_list_protocol_origin_modify(
 	struct nb_cb_modify_args *args);
 int pathd_srte_segment_list_originator_modify(struct nb_cb_modify_args *args);
@@ -31,6 +33,10 @@ int pathd_srte_segment_list_segment_nai_destroy(
 void pathd_srte_segment_list_segment_nai_apply_finish(
 	struct nb_cb_apply_finish_args *args);
 int pathd_srte_segment_list_segment_sid_value_destroy(
+	struct nb_cb_destroy_args *args);
+int pathd_srte_segment_list_segment_v6_sid_value_modify(
+	struct nb_cb_modify_args *args);
+int pathd_srte_segment_list_segment_v6_sid_value_destroy(
 	struct nb_cb_destroy_args *args);
 int pathd_srte_policy_create(struct nb_cb_create_args *args);
 int pathd_srte_policy_destroy(struct nb_cb_destroy_args *args);
@@ -53,6 +59,8 @@ pathd_srte_policy_candidate_path_get_next(struct nb_cb_get_next_args *args);
 int pathd_srte_policy_candidate_path_get_keys(struct nb_cb_get_keys_args *args);
 const void *pathd_srte_policy_candidate_path_lookup_entry(
 	struct nb_cb_lookup_entry_args *args);
+int pathd_srte_policy_candidate_path_pre_validate(
+	struct nb_cb_pre_validate_args *args);
 void pathd_srte_policy_candidate_path_bandwidth_apply_finish(
 	struct nb_cb_apply_finish_args *args);
 int pathd_srte_policy_candidate_path_bandwidth_destroy(

eddieruan-alibaba/sonic-frr

pathd/path_nb.h
@@ -20,6 +20,7 @@ const void *
 pathd_srte_segment_list_lookup_entry(struct nb_cb_lookup_entry_args *args);
 
 int pathd_srte_segment_list_segment_create(struct nb_cb_create_args *args);
+int pathd_srte_segment_list_segment_check_validate(struct nb_cb_create_args *args);
 int pathd_srte_segment_list_segment_destroy(struct nb_cb_destroy_args *args);
 int pathd_srte_segment_list_protocol_origin_modify(
 	struct nb_cb_modify_args *args);
@@ -31,6 +32,10 @@ int pathd_srte_segment_list_segment_nai_destroy(
 void pathd_srte_segment_list_segment_nai_apply_finish(
 	struct nb_cb_apply_finish_args *args);
 int pathd_srte_segment_list_segment_sid_value_destroy(
+	struct nb_cb_destroy_args *args);
+int pathd_srte_segment_list_segment_v6_sid_value_modify(
+	struct nb_cb_modify_args *args);
+int pathd_srte_segment_list_segment_v6_sid_value_destroy(
 	struct nb_cb_destroy_args *args);
 int pathd_srte_policy_create(struct nb_cb_create_args *args);
 int pathd_srte_policy_destroy(struct nb_cb_destroy_args *args);
@@ -53,6 +58,8 @@ pathd_srte_policy_candidate_path_get_next(struct nb_cb_get_next_args *args);
 int pathd_srte_policy_candidate_path_get_keys(struct nb_cb_get_keys_args *args);
 const void *pathd_srte_policy_candidate_path_lookup_entry(
 	struct nb_cb_lookup_entry_args *args);
+int pathd_srte_policy_candidate_path_check_validate(
+	struct nb_cb_create_args *args);
 void pathd_srte_policy_candidate_path_bandwidth_apply_finish(
 	struct nb_cb_apply_finish_args *args);
 int pathd_srte_policy_candidate_path_bandwidth_destroy(