guoguojia2021/frr

zebra: add sr te color in rnh register
Signed-off-by: guozhongfeng.gzf <guozhongfeng.gzf@alibaba-inc.com>

bgpd/bgp_nht.c
@@ -1130,9 +1130,8 @@ static int make_prefix(int afi, struct bgp_path_info *pi, struct prefix *p)
  */
 static void sendmsg_zebra_rnh(struct bgp_nexthop_cache *bnc, int command)
 {
-	bool exact_match = false;
-	bool resolve_via_default = false;
 	int ret;
+	uint32_t flags = 0;
 
 	if (!zclient)
 		return;
@@ -1154,9 +1153,9 @@ static void sendmsg_zebra_rnh(struct bgp_nexthop_cache *bnc, int command)
 	}
 	if (command == ZEBRA_NEXTHOP_REGISTER) {
 		if (CHECK_FLAG(bnc->flags, BGP_NEXTHOP_CONNECTED))
-			exact_match = true;
+			SET_FLAG(flags, NEXTHOP_REGISTER_FLAG_CONNECTED);
 		if (CHECK_FLAG(bnc->flags, BGP_STATIC_ROUTE_EXACT_MATCH))
-			resolve_via_default = true;
+			SET_FLAG(flags, NEXTHOP_REGISTER_FLAG_RESOLVE_VIA_DEFAULT);
 	}
 
 	if (BGP_DEBUG(zebra, ZEBRA))
@@ -1165,8 +1164,7 @@ static void sendmsg_zebra_rnh(struct bgp_nexthop_cache *bnc, int command)
 			   bnc->bgp->name_pretty);
 
 	ret = zclient_send_rnh(zclient, command, &bnc->prefix, SAFI_UNICAST,
-			       exact_match, resolve_via_default,
-			       bnc->bgp->vrf_id);
+			       flags, bnc->bgp->vrf_id, bnc->srte_color);
 	if (ret == ZCLIENT_SEND_FAILURE) {
 		flog_warn(EC_BGP_ZEBRA_SEND,
 			  "sendmsg_nexthop: zclient_send_message() failed");

lib/zclient.c
@@ -772,17 +772,15 @@ static void zclient_connect(struct event *t)
 }
 
 enum zclient_send_status zclient_send_rnh(struct zclient *zclient, int command,
-					  const struct prefix *p, safi_t safi,
-					  bool connected, bool resolve_via_def,
-					  vrf_id_t vrf_id)
+					  const struct prefix *p, safi_t safi, uint32_t flags,
+					  vrf_id_t vrf_id, uint32_t srte_color)
 {
 	struct stream *s;
 
 	s = zclient->obuf;
 	stream_reset(s);
 	zclient_create_header(s, command, vrf_id);
-	stream_putc(s, (connected) ? 1 : 0);
-	stream_putc(s, (resolve_via_def) ? 1 : 0);
+	stream_putl(s, flags);
 	stream_putw(s, safi);
 	stream_putw(s, PREFIX_FAMILY(p));
 	stream_putc(s, p->prefixlen);
@@ -796,6 +794,10 @@ enum zclient_send_status zclient_send_rnh(struct zclient *zclient, int command,
 	default:
 		break;
 	}
+
+	if (CHECK_FLAG(flags, NEXTHOP_REGISTER_FLAG_COLOR))
+		stream_putl(s, srte_color);
+
 	stream_putw_at(s, 0, stream_get_endp(s));
 
 	return zclient_send_message(zclient);

lib/zclient.h
@@ -70,6 +70,10 @@ typedef uint16_t zebra_size_t;
 #define ZEBRA_FEC_REGISTER_LABEL          0x1
 #define ZEBRA_FEC_REGISTER_LABEL_INDEX    0x2
 
+#define NEXTHOP_REGISTER_FLAG_CONNECTED           (1 << 0)
+#define NEXTHOP_REGISTER_FLAG_RESOLVE_VIA_DEFAULT (1 << 1)
+#define NEXTHOP_REGISTER_FLAG_COLOR               (1 << 2)
+
 /* Client capabilities */
 enum zserv_client_capabilities {
 	ZEBRA_CLIENT_GR_CAPABILITIES = 1,
@@ -1130,8 +1134,7 @@ extern enum zclient_send_status zclient_route_send(uint8_t, struct zclient *,
 						   struct zapi_route *);
 extern enum zclient_send_status
 zclient_send_rnh(struct zclient *zclient, int command, const struct prefix *p,
-		 safi_t safi, bool connected, bool resolve_via_default,
-		 vrf_id_t vrf_id);
+		 safi_t safi, uint32_t flags, vrf_id_t vrf_id, uint32_t srte_color);
 int zapi_nexthop_encode(struct stream *s, const struct zapi_nexthop *api_nh,
 			uint32_t api_flags, uint32_t api_message);
 extern int zapi_route_encode(uint8_t, struct stream *, struct zapi_route *);

ospfd/ospf_zebra.c
@@ -1473,6 +1473,7 @@ void ospf_zebra_import_default_route(struct ospf *ospf, bool unreg)
 {
 	struct prefix prefix = {};
 	int command;
+	uint32_t flags = 0;
 
 	if (zclient->sock < 0) {
 		if (IS_DEBUG_OSPF(zebra, ZEBRA))
@@ -1494,8 +1495,9 @@ void ospf_zebra_import_default_route(struct ospf *ospf, bool unreg)
 			   zserv_command_string(command), &prefix,
 			   ospf_vrf_id_to_name(ospf->vrf_id));
 
-	if (zclient_send_rnh(zclient, command, &prefix, SAFI_UNICAST, false,
-			     true, ospf->vrf_id) == ZCLIENT_SEND_FAILURE)
+	SET_FLAG(flags, NEXTHOP_REGISTER_FLAG_RESOLVE_VIA_DEFAULT);
+	if (zclient_send_rnh(zclient, command, &prefix, SAFI_UNICAST, flags,
+			    ospf->vrf_id, 0) == ZCLIENT_SEND_FAILURE)
 		flog_err(EC_LIB_ZAPI_SOCKET, "%s(%s): zclient_send_rnh() failed",
 			 __func__, ospf_vrf_id_to_name(ospf->vrf_id));
 }

sharpd/sharp_zebra.c
@@ -623,13 +623,17 @@ void sharp_zebra_nexthop_watch(struct prefix *p, vrf_id_t vrf_id, bool import, b
 {
 	int command = ZEBRA_NEXTHOP_REGISTER;
 	safi_t safi = mrib ? SAFI_MULTICAST : SAFI_UNICAST;
+	uint32_t flags = 0;
 
 	command = ZEBRA_NEXTHOP_REGISTER;
 
 	if (!watch)
 		command = ZEBRA_NEXTHOP_UNREGISTER;
 
-	if (zclient_send_rnh(zclient, command, p, safi, connected, false, vrf_id) ==
+	if (connected)
+		SET_FLAG(flags, NEXTHOP_REGISTER_FLAG_CONNECTED);
+
+	if (zclient_send_rnh(zclient, command, p, safi, flags, vrf_id, 0) ==
 	    ZCLIENT_SEND_FAILURE)
 		zlog_warn("%s: Failure to send nexthop to zebra", __func__);
 }

staticd/static_zebra.c
@@ -308,6 +308,7 @@ void static_zebra_nht_register(struct static_nexthop *nh, bool reg)
 	struct static_route_info *si = static_route_info_from_rnode(rn);
 	struct static_nht_data *nhtd, lookup = {};
 	uint32_t cmd;
+	uint32_t flags = 0;
 
 	if (!static_zebra_nht_get_prefix(nh, &lookup.nh))
 		return;
@@ -373,8 +374,8 @@ void static_zebra_nht_register(struct static_nexthop *nh, bool reg)
 		       "Unregistering nexthop(%pFX) for %pRN", &lookup.nh, rn);
 	}
 
-	if (zclient_send_rnh(zclient, cmd, &lookup.nh, si->safi, false, false,
-			     nh->nh_vrf_id) == ZCLIENT_SEND_FAILURE)
+	if (zclient_send_rnh(zclient, cmd, &lookup.nh, si->safi, flags,
+			     nh->nh_vrf_id, 0) == ZCLIENT_SEND_FAILURE)
 		zlog_warn("%s: Failure to send nexthop %pFX for %pRN to zebra",
 			  __func__, &lookup.nh, rn);
 	else if (reg)

zebra/zapi_msg.c
@@ -1226,12 +1226,14 @@ static void zread_rnh_register(ZAPI_HANDLER_ARGS)
 	struct stream *s;
 	struct prefix p;
 	unsigned short l = 0;
-	uint8_t connected = 0;
-	uint8_t resolve_via_default;
+	bool connected = false;
+	bool resolve_via_default = false;
 	bool exist;
 	bool flag_changed = false;
 	uint8_t orig_flags;
 	safi_t safi;
+	uint32_t flags = 0;
+	uint32_t srte_color = 0;
 
 	if (IS_ZEBRA_DEBUG_NHT)
 		zlog_debug(
@@ -1245,12 +1247,13 @@ static void zread_rnh_register(ZAPI_HANDLER_ARGS)
 		client->nh_reg_time = monotime(NULL);
 
 	while (l < hdr->length) {
-		STREAM_GETC(s, connected);
-		STREAM_GETC(s, resolve_via_default);
+		srte_color = 0;
+		flags = 0;
+		STREAM_GETL(s, flags);
 		STREAM_GETW(s, safi);
 		STREAM_GETW(s, p.family);
 		STREAM_GETC(s, p.prefixlen);
-		l += 7;
+		l += 9;
 		if (p.family == AF_INET) {
 			client->v4_nh_watch_add_cnt++;
 			if (p.prefixlen > IPV4_MAX_BITLEN) {
@@ -1278,9 +1281,23 @@ static void zread_rnh_register(ZAPI_HANDLER_ARGS)
 				p.family);
 			return;
 		}
+		if (CHECK_FLAG(flags, NEXTHOP_REGISTER_FLAG_COLOR)) {
+			STREAM_GETL(s, srte_color);
+			l += 4;
+		}
 		rnh = zebra_add_rnh(&p, zvrf_id(zvrf), safi, &exist);
 		if (!rnh)
 			return;
+
+		if (CHECK_FLAG(flags, NEXTHOP_REGISTER_FLAG_CONNECTED))
+			connected = true;
+		else
+			connected = false;
+
+		if (CHECK_FLAG(flags, NEXTHOP_REGISTER_FLAG_RESOLVE_VIA_DEFAULT))
+			resolve_via_default = true;
+		else
+			resolve_via_default = false;
 
 		orig_flags = rnh->flags;
 		if (connected && !CHECK_FLAG(rnh->flags, ZEBRA_NHT_CONNECTED))
@@ -1315,6 +1332,8 @@ static void zread_rnh_unregister(ZAPI_HANDLER_ARGS)
 	struct prefix p;
 	unsigned short l = 0;
 	safi_t safi;
+	uint32_t srte_color = 0;
+	uint32_t flags = 0;
 
 	if (IS_ZEBRA_DEBUG_NHT)
 		zlog_debug(
@@ -1325,19 +1344,18 @@ static void zread_rnh_unregister(ZAPI_HANDLER_ARGS)
 	s = msg;
 
 	while (l < hdr->length) {
-		uint8_t ignore;
+		flags = 0;
+		srte_color = 0;
 
-		STREAM_GETC(s, ignore);
-		if (ignore != 0)
-			goto stream_failure;
-		STREAM_GETC(s, ignore);
-		if (ignore != 0)
+		STREAM_GETL(s, flags);
+		if (CHECK_FLAG(flags, NEXTHOP_REGISTER_FLAG_CONNECTED)
+			|| CHECK_FLAG(flags, NEXTHOP_REGISTER_FLAG_RESOLVE_VIA_DEFAULT))
 			goto stream_failure;
 
 		STREAM_GETW(s, safi);
 		STREAM_GETW(s, p.family);
 		STREAM_GETC(s, p.prefixlen);
-		l += 7;
+		l += 9;
 		if (p.family == AF_INET) {
 			client->v4_nh_watch_rem_cnt++;
 			if (p.prefixlen > IPV4_MAX_BITLEN) {
@@ -1365,6 +1383,11 @@ static void zread_rnh_unregister(ZAPI_HANDLER_ARGS)
 				p.family);
 			return;
 		}
+		if (CHECK_FLAG(flags, NEXTHOP_REGISTER_FLAG_COLOR)) {
+			STREAM_GETL(s, srte_color);
+			l += 4;
+		}
+
 		rnh = zebra_lookup_rnh(&p, zvrf_id(zvrf), safi);
 		if (rnh) {
 			client->nh_dereg_time = monotime(NULL);

eddieruan-alibaba/sonic-frr

bgpd/bgp_nht.c
@@ -1130,9 +1130,8 @@ static int make_prefix(int afi, struct bgp_path_info *pi, struct prefix *p)
  */
 static void sendmsg_zebra_rnh(struct bgp_nexthop_cache *bnc, int command)
 {
-	bool exact_match = false;
-	bool resolve_via_default = false;
 	int ret;
+	uint8_t flags = 0;
 
 	if (!zclient)
 		return;
@@ -1154,9 +1153,9 @@ static void sendmsg_zebra_rnh(struct bgp_nexthop_cache *bnc, int command)
 	}
 	if (command == ZEBRA_NEXTHOP_REGISTER) {
 		if (CHECK_FLAG(bnc->flags, BGP_NEXTHOP_CONNECTED))
-			exact_match = true;
+			SET_FLAG(flags, NEXTHOP_REGISTER_FLAG_CONNECTED);
 		if (CHECK_FLAG(bnc->flags, BGP_STATIC_ROUTE_EXACT_MATCH))
-			resolve_via_default = true;
+			SET_FLAG(flags, NEXTHOP_REGISTER_FLAG_RESOLVE_VIA_DEFAULT);
 	}
 
 	if (BGP_DEBUG(zebra, ZEBRA))
@@ -1165,8 +1164,7 @@ static void sendmsg_zebra_rnh(struct bgp_nexthop_cache *bnc, int command)
 			   bnc->bgp->name_pretty);
 
 	ret = zclient_send_rnh(zclient, command, &bnc->prefix, SAFI_UNICAST,
-			       exact_match, resolve_via_default,
-			       bnc->bgp->vrf_id);
+			       flags, bnc->bgp->vrf_id, bnc->srte_color);
 	if (ret == ZCLIENT_SEND_FAILURE) {
 		flog_warn(EC_BGP_ZEBRA_SEND,
 			  "sendmsg_nexthop: zclient_send_message() failed");

lib/zclient.c
@@ -772,17 +772,15 @@ static void zclient_connect(struct event *t)
 }
 
 enum zclient_send_status zclient_send_rnh(struct zclient *zclient, int command,
-					  const struct prefix *p, safi_t safi,
-					  bool connected, bool resolve_via_def,
-					  vrf_id_t vrf_id)
+					  const struct prefix *p, safi_t safi, uint8_t flags,
+					  vrf_id_t vrf_id, uint32_t srte_color)
 {
 	struct stream *s;
 
 	s = zclient->obuf;
 	stream_reset(s);
 	zclient_create_header(s, command, vrf_id);
-	stream_putc(s, (connected) ? 1 : 0);
-	stream_putc(s, (resolve_via_def) ? 1 : 0);
+	stream_putl(s, flags);
 	stream_putw(s, safi);
 	stream_putw(s, PREFIX_FAMILY(p));
 	stream_putc(s, p->prefixlen);
@@ -796,6 +794,10 @@ enum zclient_send_status zclient_send_rnh(struct zclient *zclient, int command,
 	default:
 		break;
 	}
+
+	if (CHECK_FLAG(flags, NEXTHOP_REGISTER_FLAG_COLOR))
+		stream_putl(s, srte_color);
+
 	stream_putw_at(s, 0, stream_get_endp(s));
 
 	return zclient_send_message(zclient);

lib/zclient.h
@@ -70,6 +70,10 @@ typedef uint16_t zebra_size_t;
 #define ZEBRA_FEC_REGISTER_LABEL          0x1
 #define ZEBRA_FEC_REGISTER_LABEL_INDEX    0x2
 
+#define NEXTHOP_REGISTER_FLAG_CONNECTED           (1 << 0)
+#define NEXTHOP_REGISTER_FLAG_RESOLVE_VIA_DEFAULT (1 << 1)
+#define NEXTHOP_REGISTER_FLAG_COLOR               (1 << 2)
+
 /* Client capabilities */
 enum zserv_client_capabilities {
 	ZEBRA_CLIENT_GR_CAPABILITIES = 1,
@@ -1130,8 +1134,7 @@ extern enum zclient_send_status zclient_route_send(uint8_t, struct zclient *,
 						   struct zapi_route *);
 extern enum zclient_send_status
 zclient_send_rnh(struct zclient *zclient, int command, const struct prefix *p,
-		 safi_t safi, bool connected, bool resolve_via_default,
-		 vrf_id_t vrf_id);
+		 safi_t safi, uint8_t flags, vrf_id_t vrf_id, uint32_t srte_color);
 int zapi_nexthop_encode(struct stream *s, const struct zapi_nexthop *api_nh,
 			uint32_t api_flags, uint32_t api_message);
 extern int zapi_route_encode(uint8_t, struct stream *, struct zapi_route *);

ospfd/ospf_zebra.c
@@ -1473,6 +1473,7 @@ void ospf_zebra_import_default_route(struct ospf *ospf, bool unreg)
 {
 	struct prefix prefix = {};
 	int command;
+	uint8_t flags = 0;
 
 	if (zclient->sock < 0) {
 		if (IS_DEBUG_OSPF(zebra, ZEBRA))
@@ -1494,8 +1495,9 @@ void ospf_zebra_import_default_route(struct ospf *ospf, bool unreg)
 			   zserv_command_string(command), &prefix,
 			   ospf_vrf_id_to_name(ospf->vrf_id));
 
-	if (zclient_send_rnh(zclient, command, &prefix, SAFI_UNICAST, false,
-			     true, ospf->vrf_id) == ZCLIENT_SEND_FAILURE)
+	SET_FLAG(flags, NEXTHOP_REGISTER_FLAG_RESOLVE_VIA_DEFAULT);
+	if (zclient_send_rnh(zclient, command, &prefix, SAFI_UNICAST, flags,
+			    ospf->vrf_id, 0) == ZCLIENT_SEND_FAILURE)
 		flog_err(EC_LIB_ZAPI_SOCKET, "%s(%s): zclient_send_rnh() failed",
 			 __func__, ospf_vrf_id_to_name(ospf->vrf_id));
 }

sharpd/sharp_zebra.c
@@ -623,13 +623,17 @@ void sharp_zebra_nexthop_watch(struct prefix *p, vrf_id_t vrf_id, bool import, b
 {
 	int command = ZEBRA_NEXTHOP_REGISTER;
 	safi_t safi = mrib ? SAFI_MULTICAST : SAFI_UNICAST;
+	uint8_t flags = 0;
 
 	command = ZEBRA_NEXTHOP_REGISTER;
 
 	if (!watch)
 		command = ZEBRA_NEXTHOP_UNREGISTER;
 
-	if (zclient_send_rnh(zclient, command, p, safi, connected, false, vrf_id) ==
+	if (connected)
+		SET_FLAG(flags, NEXTHOP_REGISTER_FLAG_CONNECTED);
+
+	if (zclient_send_rnh(zclient, command, p, safi, flags, vrf_id, 0) ==
 	    ZCLIENT_SEND_FAILURE)
 		zlog_warn("%s: Failure to send nexthop to zebra", __func__);
 }

staticd/static_zebra.c
@@ -308,6 +308,7 @@ void static_zebra_nht_register(struct static_nexthop *nh, bool reg)
 	struct static_route_info *si = static_route_info_from_rnode(rn);
 	struct static_nht_data *nhtd, lookup = {};
 	uint32_t cmd;
+	uint8_t flags = 0;
 
 	if (!static_zebra_nht_get_prefix(nh, &lookup.nh))
 		return;
@@ -373,8 +374,8 @@ void static_zebra_nht_register(struct static_nexthop *nh, bool reg)
 		       "Unregistering nexthop(%pFX) for %pRN", &lookup.nh, rn);
 	}
 
-	if (zclient_send_rnh(zclient, cmd, &lookup.nh, si->safi, false, false,
-			     nh->nh_vrf_id) == ZCLIENT_SEND_FAILURE)
+	if (zclient_send_rnh(zclient, cmd, &lookup.nh, si->safi, flags,
+			     nh->nh_vrf_id, 0) == ZCLIENT_SEND_FAILURE)
 		zlog_warn("%s: Failure to send nexthop %pFX for %pRN to zebra",
 			  __func__, &lookup.nh, rn);
 	else if (reg)

zebra/zapi_msg.c
@@ -1233,12 +1233,14 @@ static void zread_rnh_register(ZAPI_HANDLER_ARGS)
 	struct stream *s;
 	struct prefix p;
 	unsigned short l = 0;
-	uint8_t connected = 0;
-	uint8_t resolve_via_default;
+	bool connected = false;
+	bool resolve_via_default = false;
 	bool exist;
 	bool flag_changed = false;
 	uint8_t orig_flags;
 	safi_t safi;
+	uint8_t flags = 0;
+	uint32_t srte_color = 0;
 
 	if (IS_ZEBRA_DEBUG_NHT)
 		zlog_debug(
@@ -1252,12 +1254,13 @@ static void zread_rnh_register(ZAPI_HANDLER_ARGS)
 		client->nh_reg_time = monotime(NULL);
 
 	while (l < hdr->length) {
-		STREAM_GETC(s, connected);
-		STREAM_GETC(s, resolve_via_default);
+		srte_color = 0;
+
+		STREAM_GETL(s, flags);
 		STREAM_GETW(s, safi);
 		STREAM_GETW(s, p.family);
 		STREAM_GETC(s, p.prefixlen);
-		l += 7;
+		l += 9;
 		if (p.family == AF_INET) {
 			client->v4_nh_watch_add_cnt++;
 			if (p.prefixlen > IPV4_MAX_BITLEN) {
@@ -1285,9 +1288,23 @@ static void zread_rnh_register(ZAPI_HANDLER_ARGS)
 				p.family);
 			return;
 		}
+		if (CHECK_FLAG(flags, NEXTHOP_REGISTER_FLAG_COLOR)) {
+			STREAM_GETL(s, srte_color);
+			l += 4;
+		}
 		rnh = zebra_add_rnh(&p, zvrf_id(zvrf), safi, &exist);
 		if (!rnh)
 			return;
+
+		if (CHECK_FLAG(flags, NEXTHOP_REGISTER_FLAG_CONNECTED))
+			connected = true;
+		else
+			connected = false;
+
+		if (CHECK_FLAG(flags, NEXTHOP_REGISTER_FLAG_RESOLVE_VIA_DEFAULT))
+			resolve_via_default = true;
+		else
+			resolve_via_default = false;
 
 		orig_flags = rnh->flags;
 		if (connected && !CHECK_FLAG(rnh->flags, ZEBRA_NHT_CONNECTED))
@@ -1322,6 +1339,8 @@ static void zread_rnh_unregister(ZAPI_HANDLER_ARGS)
 	struct prefix p;
 	unsigned short l = 0;
 	safi_t safi;
+	uint32_t srte_color = 0;
+	uint8_t flags = 0;
 
 	if (IS_ZEBRA_DEBUG_NHT)
 		zlog_debug(
@@ -1332,19 +1351,17 @@ static void zread_rnh_unregister(ZAPI_HANDLER_ARGS)
 	s = msg;
 
 	while (l < hdr->length) {
-		uint8_t ignore;
+		srte_color = 0;
 
-		STREAM_GETC(s, ignore);
-		if (ignore != 0)
-			goto stream_failure;
-		STREAM_GETC(s, ignore);
-		if (ignore != 0)
+		STREAM_GETL(s, flags);
+		if (CHECK_FLAG(flags, NEXTHOP_REGISTER_FLAG_CONNECTED)
+			|| CHECK_FLAG(flags, NEXTHOP_REGISTER_FLAG_RESOLVE_VIA_DEFAULT))
 			goto stream_failure;
 
 		STREAM_GETW(s, safi);
 		STREAM_GETW(s, p.family);
 		STREAM_GETC(s, p.prefixlen);
-		l += 7;
+		l += 9;
 		if (p.family == AF_INET) {
 			client->v4_nh_watch_rem_cnt++;
 			if (p.prefixlen > IPV4_MAX_BITLEN) {
@@ -1372,6 +1389,11 @@ static void zread_rnh_unregister(ZAPI_HANDLER_ARGS)
 				p.family);
 			return;
 		}
+		if (CHECK_FLAG(flags, NEXTHOP_REGISTER_FLAG_COLOR)) {
+			STREAM_GETL(s, srte_color);
+			l += 4;
+		}
+
 		rnh = zebra_lookup_rnh(&p, zvrf_id(zvrf), safi);
 		if (rnh) {
 			client->nh_dereg_time = monotime(NULL);