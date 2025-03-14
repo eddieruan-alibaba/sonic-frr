2c651a41add40dd171267a1558a3c476450a3ce4
Yuqing Zhao <galadriel.zyq@alibaba-inc.com>
2025-03-14 09:20:21 +0000

    bgpd, zebra: fix synchronization problem caused by configuration order

--- zebra/zapi_msg.h
+++ zebra/zapi_msg.h
@@ -100,6 +100,9 @@ extern void zsend_srv6_sid_notify(struct zserv *client,
 				  uint32_t wide_func, const char *locator_name,
 				  enum zapi_srv6_sid_notify note);
 
+extern void zsend_srv6_static_sid_update();
+extern void zsend_srv6_static_sid_notify();
+
 extern int zsend_client_close_notify(struct zserv *client,
 				     struct zserv *closed_client);
 
