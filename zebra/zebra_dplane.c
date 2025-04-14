c40dfbc6963bfb996e515126c046fa51a50656b2
Eddie Ruan <eddie.ruan@alibaba-inc.com>
2025-04-14 13:45:35 -0700

    Add debug

--- zebra/zebra_dplane.c
+++ zebra/zebra_dplane.c
@@ -4560,7 +4560,7 @@ dplane_nexthop_update_internal(struct nhg_hash_entry *nhe, enum dplane_op_e op)
 			SET_FLAG(flags, ZEBRA_FLAG_KERNEL_BYPASS);
 		}
 		dplane_ctx_set_flags(ctx, flags);
-		zlog_err("dplane_nexthop_update_internal : id %d set flags %x", nhe->id, flags);
+		zlog_info("dplane_nexthop_update_internal : id %d op %d, set flags %x", nhe->id, (uint32_t) op, flags);
 
 		ret = dplane_update_enqueue(ctx);
 	}
